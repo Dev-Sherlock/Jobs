ded5033890afaaacf7d27f7de810dca649f29a0e
proger-xyz <khanbayov.tamerlan@gmail.com>
2021-04-18 16:07:11 +0200

    added crud

--- Jobs/api_calls/urls.py
+++ Jobs/api_calls/urls.py
@@ -4,7 +4,10 @@ from .views import *
 urlpatterns = [
     path('', index, name="index"),
     path('<int:id>/', post, name="post"),
-
+    # path('<str:pk>/', JobDetail.as_view(), name='job'),
+    path('job-create/', JobCreate.as_view(), name='job-create'),
+    path('job-update/<str:pk>/', JobUpdate.as_view(), name='job-update'),
+    path('job-delete/<str:pk>/', JobDelete.as_view(), name='job-delete'),
 ]
 
 
--- Jobs/api_calls/views.py
+++ Jobs/api_calls/views.py
@@ -3,8 +3,11 @@ from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from .models import Job
-from django.views.generic import TemplateView, ListView
-
+from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
@@ -26,3 +29,28 @@ def post(request,id):
     context={'job': job}
     return render(request, 'api_calls/post.html',context)
 
+
+
+class JobCreate(CreateView):
+    model = Job
+    fields = ['title','description','author']
+    template_name = 'api_calls/job_create.html'
+    success_url = reverse_lazy('index')
+
+
+class JobUpdate(UpdateView):
+    model = Job
+    fields = ['title','description','author']
+    template_name = 'api_calls/job_update.html'
+    success_url = reverse_lazy('index')
+
+class JobDelete(DeleteView):
+    model = Job
+    context_object_name = 'task'
+    success_url = reverse_lazy('index')
+
+
+class ContactFormView(FormView):
+    template_name = 'job_update.html'
+    form_class = Job
+    success_url = reverse_lazy('index')
